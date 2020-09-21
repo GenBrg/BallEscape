6b855061faeb5fd1693d814c6c48c663be5b75de
wrystal <haowanghh@gmail.com>
2020-09-21 12:23:30 -0400

    update ball

---
+++ Ball.hpp
@@ -0,0 +1,17 @@
+//
+// Created by hao on 9/21/20.
+//
+
+#include "Platform.hpp"
+
+#ifndef INC_15_466_F20_BASE1_BALL_H
+#define INC_15_466_F20_BASE1_BALL_H
+
+struct Ball {
+    // which platform it belongs to
+    Platform* platform;
+
+
+};
+
+#endif //INC_15_466_F20_BASE1_BALL_H
